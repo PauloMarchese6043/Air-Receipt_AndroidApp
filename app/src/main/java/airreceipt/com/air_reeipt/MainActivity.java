eb3896efdb28490fcd431fd2a277732b0afbd37c
cindy0502 <cindy.ng1989@gmail.com>
2018-08-22 13:18:51 +1000

    Little test

--- app/src/main/java/airreceipt/com/air_reeipt/MainActivity.java
+++ app/src/main/java/airreceipt/com/air_reeipt/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //Creating the menu and Search--->NOT DONE
 
 //        @Override
